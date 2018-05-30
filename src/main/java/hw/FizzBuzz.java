2eeb41d94b9c624f5d7dbb516046695853ab3454
Devin <zayas_d91@yahoo.com>
2018-05-30 18:36:44 -0500

    Lab1-part2 corrected

--- src/main/java/hw/FizzBuzz.java
+++ src/main/java/hw/FizzBuzz.java
@@ -5,6 +5,43 @@ package hw;
 
 public class FizzBuzz {
 
+    public static void fizzBuzz(int m) {
+
+
+        for (int i = 1; i <= m; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizz Buzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else
+                System.out.println(i);
+
+
+        }
+    }
+
+
+
+
+
+
+
+//    public static String[] fizzBuzz(final int n) {
+//        String[] myString = new String[n];
+//        for (int i=1; i<=n; i++){
+//            if ( i%3==0 && i%5==0){
+//                myString[i-1]= "FizzBuzz";
+//            }else if (i%3==0){
+//                myString[i-1]= "Fizz";
+//            }else if (i%5==0){
+//                myString[i-1]="Buzz";
+//            }else
+//                myString[i-1]=String.valueOf(i);
+//        }
+//        return myString;
+//    }
 
 
 
--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -1,7 +1,6 @@
 
 package hw;
-
-
+import java.util.Arrays;
 public class Main {
 
 
@@ -12,7 +11,7 @@ public class Main {
     if (args.length > 0) {
       try {
         max = Integer.parseInt(args[0]);
-        if (max<0){
+        if (max<=0){
           System.out.println("Enter a positive integer");
         }
 
@@ -26,19 +25,10 @@ public class Main {
        *and returns to the OS.  So it’s more of a status message to you.
        */
     }
-
-    for (int i=1; i<=max; i++){
-      if ( i%3==0 && i%5==0) {
-        System.out.println("Fizz Buzz");
-      } else if (i%5==0) {
-        System.out.println("Buzz");
-      } else if (i%3==0) {
-        System.out.println("Fizz");
-      } else
-        System.out.println(i);
+    FizzBuzz.fizzBuzz(max);
 
 
-    }
+//    System.out.println(Arrays.deepToString(FizzBuzz.fizzBuzz(max)));
 
 
 
